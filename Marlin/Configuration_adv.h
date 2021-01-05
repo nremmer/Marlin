d91607f66587c2f070b35aa26ed51d53c4d74e11
Nate Emmer <nemmer@gmail.com>
2021-01-04 18:30:17 -0700

    Updates

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -802,7 +802,7 @@
 #if ENABLED(ASSISTED_TRAMMING)
 
   // Define positions for probe points.
-  #define TRAMMING_POINT_XY { {  20, 20 }, { 180,  20 }, { 180, 180 }, { 20, 180 } }
+  #define TRAMMING_POINT_XY { {  40, 20 }, { 268,  20 }, { 268, 268 }, { 40, 268 } }
 
   // Define position names for probe points.
   #define TRAMMING_POINT_NAME_1 "Front-Left"
